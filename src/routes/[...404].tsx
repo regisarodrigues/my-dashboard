a20cc0cdf6587f2bf6579734d7c3b88e9ba89104
Regis Rodrigues <regisarodrigues@hotmail.com>
2025-11-25 10:00:55 -0300

    feat: Initialize SolidStart project with basic routing, Tailwind CSS, and SolidStart configuration.

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,18 @@
+import { Title } from '@solidjs/meta';
+import { HttpStatusCode } from '@solidjs/start';
+
+export default function NotFound() {
+  return (
+    <main>
+      <Title>Not Found</Title>
+      <HttpStatusCode code={404} />
+      <h1>Page Not Found</h1>
+      <p>
+        Visit{' '}
+        <a href='/' target='_blank'>
+          Home
+        </a>{' '}
+      </p>
+    </main>
+  );
+}
